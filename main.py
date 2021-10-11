c7189972719b9f35aec9e78f72ac1f311eb10138
Steeldune <nighttagle@gmail.com>
2021-10-11 10:19:19 +0200

    Added Notebook

--- main.py
+++ main.py
@@ -8,9 +8,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+class ParticleBrown:
+    def init(self, startCoords, endCoords):
+        self.dim = len(startCoords)
+        self.poss = np.zeros(self.dim)
+        for pos in self.poss:
+            pos =
+
+    def increment(self):
+
+
+class DLATRee:
+    def init(self, ini_coords):
+        self.start_coods = ini_coords
+
+
+
+
+def increment_particles(coords):
+    random.random()
+
+
+def check_tree(coords):
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    origin = np.array([0.0,0.0])
+    final_coords = np.array([10.0, 10.0])
+
+    Jerry = ParticleBrown()
+    print(Jerry.dim)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
