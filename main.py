b8a4391c23727d4619b05133817ae7509c9853e1
Steeldune <nighttagle@gmail.com>
2021-10-12 11:14:19 +0200

    Started Simple Brownian Motion

--- main.py
+++ main.py
@@ -12,10 +12,23 @@ class ParticleBrown:
     def init(self, startCoords, endCoords):
         self.dim = len(startCoords)
         self.poss = np.zeros(self.dim)
-        for pos in self.poss:
-            pos =
+        self.r = 1
+        for i in range(self.dim):
+            self.poss[i] = random.uniform(startCoords[i], endCoords[i])
+        return self.poss
 
     def increment(self):
+        if self.dim == 2:
+            self.angle = random.uniform(0, 2*np.pi)
+            self.dx = np.cos(self.angle) * self.r
+            self.dy = np.sin(self.angle) * self.r
+        elif self.dim == 3:
+            self.theta = random.uniform(0, 2*np.pi)
+            self.phi = random.uniform(0, np.pi)
+            self.dx = self.r * np.sin(self.phi) * np.cos(self.theta)
+            self.dy = self.r * np.sin(self.phi) * np.sin(self.theta)
+            self.dz = self.r * np.cos(self.phi)
+
 
 
 class DLATRee:
