317292f202dd927f4082d968c2636fb1def177dd
Steeldune <nighttagle@gmail.com>
2021-10-22 16:40:50 +0200

    Tried to implement 3d, will work on it later

--- main.py
+++ main.py
@@ -31,13 +31,12 @@ class ParticleBrown:
 
 
 class Brown2D(ParticleBrown):
-    def __init__(self, startCoords, endCoords, rand_len=512, r=1, spawn_r = 0):
+    def __init__(self, startCoords, endCoords, rand_len=512, r=1, spawn_r=0):
         super().__init__(startCoords, endCoords, rand_len, r)
         self.dim = 2
-        self.pos_angle = np.random.uniform(0, 2*np.pi)
+        self.pos_angle = np.random.uniform(0, 2 * np.pi)
         self.pos[0] = np.cos(self.pos_angle) * spawn_r
         self.pos[1] = np.sin(self.pos_angle) * spawn_r
-
 
     def gen_rand(self):
         self.angle_array = np.random.uniform(0, 2 * np.pi, self.rand_int)
@@ -45,9 +44,15 @@ class Brown2D(ParticleBrown):
 
 
 class Brown3D(ParticleBrown):
-    def __init__(self, startCoords, endCoords, rand_len=512, r=1):
+    def __init__(self, startCoords, endCoords, rand_len=512, r=1, spawn_r=0):
         super().__init__(startCoords, endCoords, rand_len, r)
         self.dim = 3
+        self.pos_theta = np.random.uniform(0, 2 * np.pi)
+        self.pos_phi = np.random.uniform(0, np.pi)
+
+        self.pos[0] = spawn_r * np.sin(self.pos_phi) * np.cos(self.pos_theta)
+        self.pos[1] = spawn_r * np.sin(self.pos_phi) * np.sin(self.pos_theta)
+        self.pos[2] = spawn_r + np.cos(self.pos_phi)
 
     def gen_rand(self):
         self.angle_array = np.random.uniform(0, 2 * np.pi, (2, self.rand_int))
@@ -68,7 +73,7 @@ class DLATRee:
         for seed in self.seed_array:
             temp_path = np.zeros((len(seed), len(path[0])))
             for i in range(len(seed)):
-                temp_path[i] = path[i]-seed[i]
+                temp_path[i] = path[i] - seed[i]
             temp_path = np.linalg.norm(temp_path, axis=0)
             check_path = np.where(abs(temp_path) < self.r, True, False)
             seed_indices = np.where(check_path == 1)[0]
@@ -81,20 +86,21 @@ class DLATRee:
         return len(self.seed_array)
 
 
-
 def increment_particles(coords):
     random.random()
     return None
 
+
 if __name__ == '__main__':
-    origin = np.array([-5.0, -5.0])
-    final_coords = np.array([5.0, 5.0])
-    George = DLATRee([0.0, 0.0], r=0.2)
+    origin = np.array([-5.0, -5.0, -5.0])
+    final_coords = np.array([5.0, 5.0, 5.0])
+    George = DLATRee([0.0, 0.0, 0.0], r=0.2)
     spawn_r = 3
     max_dist = 0.0
 
-    for j in tqdm(range(100)):
-        Brownian_array = [Brown2D(origin, [10, 0.00100], rand_len=512, r=.1, spawn_r = (max_dist + 1)) for i in range(100)]
+    for j in tqdm(range(300)):
+        Brownian_array = [Brown3D(origin, [10, 10, 0.00100], rand_len=512, r=.1, spawn_r=(max_dist + 1)) for i in
+                          range(10)]
 
         for i in Brownian_array:
             i.gen_rand()
@@ -106,12 +112,16 @@ if __name__ == '__main__':
                 seed_l = np.linalg.norm(i.path[:, indices[0]])
                 max_dist = np.maximum(max_dist, np.linalg.norm(i.path[:, indices[0]]))
 
-
-
     colors = iter(cm.rainbow(np.linspace(0, 1, len(George.seed_array))))
 
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
     for i in range(len(George.seed_array)):
-        plt.scatter(George.seed_array[i][0], George.seed_array[i][1], s=30, color=next(colors), alpha=1)
+        ax.scatter(George.seed_array[i][0], George.seed_array[i][1], George.seed_array[i][2], s=30, color=next(colors), alpha=1)
+
+    # for i in range(len(George.seed_array)):
+    #     plt.scatter(George.seed_array[i][0], George.seed_array[i][1], s=30, color=next(colors), alpha=1)
     # plt.xlim(-5, 5)
     # plt.ylim(-5, 5)
 
