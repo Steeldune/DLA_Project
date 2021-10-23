c9a1f98b1fde78acaab9fa03c106d5142adba52f
Simon Steeldune <nighttagle@gmail.com>
2021-10-23 12:20:33 +0200

    Updated DLA3DTree to support 3d trees

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Brown2D(ParticleBrown):
     def __init__(self, startCoords, endCoords, rand_len=512, r=1, spawn_r=0):
         super().__init__(startCoords, endCoords, rand_len, r)
         self.dim = 2
-        self.pos_angle = np.random.uniform(0, 2 * np.pi)
+        self.pos_angle = np.random.uniform(0, 2*np.pi)
         self.pos[0] = np.cos(self.pos_angle) * spawn_r
         self.pos[1] = np.sin(self.pos_angle) * spawn_r
 
@@ -62,15 +62,20 @@ class Brown3D(ParticleBrown):
                            self.r * np.cos(self.angle_array[1])])
 
 
-class DLATRee:
+class DLATree:
     def __init__(self, ini_coords, r=0.1):
         self.seed_array = np.array([ini_coords])
-        self.grow_array = []
         self.r = r
+        self.link_tree = []
+        self.connect_node = 0
+
+        self.link_fig = plt.figure()
+        self.link_ax = self.link_fig.add_subplot()
 
     def check_path(self, path):
-        check_indices = np.array([], dtype=int)
-        for seed in self.seed_array:
+        check_indices = -1
+        self.connect_node = -1
+        for k, seed in enumerate(self.seed_array):
             temp_path = np.zeros((len(seed), len(path[0])))
             for i in range(len(seed)):
                 temp_path[i] = path[i] - seed[i]
@@ -78,23 +83,40 @@ class DLATRee:
             check_path = np.where(abs(temp_path) < self.r, True, False)
             seed_indices = np.where(check_path == 1)[0]
             if len(seed_indices) > 0:
-                check_indices = np.append(check_indices, seed_indices[0])
-        return np.sort(check_indices)
+                if check_indices == -1:
+                    check_indices = seed_indices[0]
+                    self.connect_node = k
+                elif seed_indices[0] < check_indices:
+                    check_indices = seed_indices[0]
+                    self.connect_node = k
+        if self.connect_node > -1:
+            self.link_tree.append([self.connect_node, np.shape(self.seed_array)[0]])
+        return check_indices
 
     def add_seed(self, coords):
         self.seed_array = np.append(self.seed_array, [coords], axis=0)
         return len(self.seed_array)
 
+    def gen_link_graph(self, colors):
+        for i in range(len(self.link_tree)):
+            self.link_ax.scatter(self.link_tree[i][1], self.link_tree[i][0], color=colors)
 
-def increment_particles(coords):
-    random.random()
-    return None
+class DLA3DTree(DLATree):
+    def __init__(self, ini_coords, r=0.1):
+        super().__init__(ini_coords, r)
+        self.tree_fig = plt.figure()
+        self.tree_ax = self.tree_fig.add_subplot(projection='3d')
+
+    def gen_tree_graph(self, colors):
+        for i in range(len(self.seed_array)):
+            self.tree_ax.scatter(self.seed_array[i][0], self.seed_array[i][1], self.seed_array[i][2], s=30, color=next(colors), alpha=1)
+
 
 
 if __name__ == '__main__':
     origin = np.array([-5.0, -5.0, -5.0])
     final_coords = np.array([5.0, 5.0, 5.0])
-    George = DLATRee([0.0, 0.0, 0.0], r=0.2)
+    George = DLA3DTree([0.0, 0.0, 0.0], r=0.2)
     spawn_r = 3
     max_dist = 0.0
 
@@ -107,23 +129,32 @@ if __name__ == '__main__':
             path = i.gen_path()
             # plt.scatter(i.path[0], i.path[1], s=40, alpha=0.5)
             indices = George.check_path(path)
-            if len(indices) > 0:
-                George.add_seed(i.path[:, indices[0]])
-                seed_l = np.linalg.norm(i.path[:, indices[0]])
-                max_dist = np.maximum(max_dist, np.linalg.norm(i.path[:, indices[0]]))
+            if indices > -1:
+                George.add_seed(i.path[:, indices])
+                seed_l = np.linalg.norm(i.path[:, indices])
+                max_dist = np.maximum(max_dist, np.linalg.norm(i.path[:, indices]))
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(George.seed_array))))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+    George.gen_tree_graph(colors)
+    George.gen_link_graph('black')
 
-    for i in range(len(George.seed_array)):
-        ax.scatter(George.seed_array[i][0], George.seed_array[i][1], George.seed_array[i][2], s=30, color=next(colors), alpha=1)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # fig2 = plt.figure()
+    # ax2 = fig2.add_subplot()
+
+    # for i in range(len(George.seed_array)):
+    #     ax.scatter(George.seed_array[i][0], George.seed_array[i][1], George.seed_array[i][2], s=30, color=next(colors), alpha=1)
+    #     if i < len(George.seed_array)-1:
+    #         ax2.scatter(George.link_tree[i][1], George.link_tree[i][0], color='black')
 
     # for i in range(len(George.seed_array)):
     #     plt.scatter(George.seed_array[i][0], George.seed_array[i][1], s=30, color=next(colors), alpha=1)
     # plt.xlim(-5, 5)
     # plt.ylim(-5, 5)
+    print(George.link_tree)
 
-    print(George.seed_array)
+
+
     plt.show()
