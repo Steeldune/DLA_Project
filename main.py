f1bf73f1ce42216cb2964d4d56c45a6f676b38fc
Simon Steeldune <nighttagle@gmail.com>
2021-11-03 20:02:44 +0100

    Added report

--- main.py
+++ main.py
@@ -64,11 +64,11 @@ class Brown3D(ParticleBrown):
         self.pos[2] = spawn_r + np.cos(self.pos_phi)
 
     def gen_rand(self):
-        self.angle_array[0] = np.random.uniform(0, 2 * np.pi)
-        self.angle_array[1] = np.arccos(np.random.uniform(-1, 1))
-        self.d = np.array([self.r * np.sin(self.angle_array[1]) * np.cos(self.angle_array[0]),
-                           self.r * np.sin(self.angle_array[1]) * np.sin(self.angle_array[0]),
-                           self.r * np.cos(self.angle_array[1])])
+        self.angles_theta = np.random.uniform(0, 2 * np.pi, self.rand_int)
+        self.angles_phi = np.arccos(np.random.uniform(-1, 1, self.rand_int))
+        self.d = np.array([self.r * np.sin(self.angles_phi) * np.cos(self.angles_theta),
+                           self.r * np.sin(self.angles_phi) * np.sin(self.angles_theta),
+                           self.r * np.cos(self.angles_phi)])
 
 
 class DLATree:
@@ -122,80 +122,59 @@ class DLA3DTree(DLATree):
 
     def gen_tree_graph(self, colors):
         for i in range(len(self.seed_array)):
-            self.tree_ax.scatter(self.seed_array[i][0], self.seed_array[i][1], self.seed_array[i][2], s=30, color=next(colors), alpha=1)
+            self.tree_ax.scatter(self.seed_array[i][0], self.seed_array[i][1], self.seed_array[i][2], s=30,
+                                 color=next(colors), alpha=1)
 
 
 if __name__ == '__main__':
-    origin = np.array([-3, 0.6])
-    final_coords = np.array([3, 0.7])
-    z_plane = 0
-    r = 4
-
-    grid_nr = 40
-
-    theta_grid = np.linspace(0, 2*np.pi, grid_nr)
-    x_grid = r * np.cos(theta_grid)
-    y_grid = r * np.sin(theta_grid)
-
-    start_seeds = np.zeros((grid_nr, 2))
-    count = 0
-    for i in range(grid_nr):
-        start_seeds[count, 0] = x_grid[i]
-        start_seeds[count, 1] = -1
-        count += 1
-
-    George = DLATree([[0.0, 0.0]], r=0.1)
     spawn_r = 3
     max_dist = 0.0
 
-    for j in tqdm(range(700)):
-        Brownian_array = [Brown2D(origin, [10, 10], rand_len=512, r=.08) for i in
+    Peter = DLA3DTree([0.0, 0.0, 0.0])
+
+    for j in tqdm(range(100)):
+        Brownian_array = [Brown3D([1.0, 1.0, 1.0], [2.0, 2.0, 2.0], rand_len=512, r=.08) for i in
                           range(10)]
 
         for i in Brownian_array:
-            i.start_pos_circle(max_dist)
+            i.start_pos_sphere(max_dist)
             i.gen_rand()
             path = i.gen_path()
+            print(i.d)
             # plt.scatter(i.path[0], i.path[1], s=40, alpha=0.5)
-            indices = George.check_path(path)
+            indices = Peter.check_path(path)
             if indices > -1:
-                George.add_seed(i.path[:, indices])
+                Peter.add_seed(i.path[:, indices])
 
                 # max_z = np.amax(George.seed_array, axis=0)[2]
                 # origin[2] = max_z
                 # final_coords[2] = max_z
                 seed_l = np.linalg.norm(i.path[:, indices])
                 max_dist = np.maximum(max_dist, np.linalg.norm(i.path[:, indices]))
-                George.adjust_origin()
+                Peter.adjust_origin()
 
-    colors = iter(cm.rainbow(np.linspace(0, 1, len(George.seed_array))))
+    colors = iter(cm.rainbow(np.linspace(0, 1, len(Peter.seed_array))))
 
     fig = plt.figure()
     ax = fig.add_subplot()
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot()
 
-    for i in range(len(George.seed_array)):
-        ax.scatter(George.seed_array[i][0], George.seed_array[i][1], s=30, color=next(colors), alpha=1)
-        if i < len(George.link_tree):
-            ax2.scatter(George.link_tree[i][1], George.link_tree[i][0], color='black')
+    for i in range(len(Peter.seed_array)):
+        ax.scatter(Peter.seed_array[i][0], Peter.seed_array[i][1], s=30, color=next(colors), alpha=1)
 
-    # for i in range(len(George.seed_array)):
-    #     plt.scatter(George.seed_array[i][0], George.seed_array[i][1], s=30, color=next(colors), alpha=1)
     plt.xlim(-5, 5)
     plt.ylim(-5, 5)
 
-    George.gen_link_graph('black')
+    Peter.gen_link_graph('black')
 
     plt.show()
 
-    # with open('coords.txt', 'w') as f:
-    #     f.write(str(np.shape(George.seed_array)[0]) + '\n')
-    #     f.write(str(np.shape(George.link_tree)[0]))
-    #     f.write('\n')
-    #     for coord in George.seed_array:
-    #         f.write(str(coord))
-    #         f.write('\n')
-    #     for link in George.link_tree:
-    #         f.write(str(link))
-    #         f.write('\n')
+    with open('coords.txt', 'w') as f:
+        f.write(str(np.shape(Peter.seed_array)[0]) + '\n')
+        f.write(str(np.shape(Peter.link_tree)[0]))
+        f.write('\n')
+        for coord in Peter.seed_array:
+            f.write(str(coord))
+            f.write('\n')
+        for link in Peter.link_tree:
+            f.write(str(link))
+            f.write('\n')
